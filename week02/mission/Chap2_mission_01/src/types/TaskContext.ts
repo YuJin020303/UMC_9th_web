59f6007553c930c179de2c15717f4bea536a6383
YuJin020303 <ekfzha8020@gmail.com>
2025-09-27 16:55:24 +0900

    feat: context 타입 정의

---
+++ week02/mission/Chap2_mission_01/src/types/TaskContext.ts
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import type { TTask } from "../types/task";
+
+// Context의 타입 정의
+interface TaskContextType {
+  todoInput: string;
+  todoList: TTask[];
+  doneList: TTask[];
+  setTodoInput: React.Dispatch<React.SetStateAction<string>>;
+  setTodoList: React.Dispatch<React.SetStateAction<TTask[]>>;
+  setDoneList: React.Dispatch<React.SetStateAction<TTask[]>>;
+}
+
+// Context 생성 (초기값은 undefined로 설정)
+export const TaskContext = createContext<TaskContextType | undefined>(
+  undefined
+);
