26112d97588d85f6372401c70a1b9523789dd7e6
YuJin020303 <ekfzha8020@gmail.com>
2025-09-27 18:38:14 +0900

    feat: 다크 모드 설정 기능 추가

--- week02/mission/Chap2_mission_02/src/components/Toggle.tsx
+++ week02/mission/Chap2_mission_02/src/components/Toggle.tsx
@@ -1,4 +1,7 @@
+import { useTask } from "../hooks/useTask";
+
 export const Toggle = () => {
+  const { isDarkMode, setIsDarkMode } = useTask();
   return (
     <div className="flex flex-col mb-5">
       <div className="flex items-center gap-2">
@@ -6,8 +9,12 @@ export const Toggle = () => {
           id="switch-link"
           type="checkbox"
           className="appearance-none relative inline-block rounded-full w-12 h-6 cursor-pointer before:inline-block before:absolute before:top-0 before:left-0 before:w-full before:h-full before:rounded-full before:bg-stone-200 before:transition-colors before:duration-200 before:ease-in after:absolute after:top-2/4 after:left-0 after:-translate-y-2/4 after:w-6 after:h-6 after:border after:border-stone-200 after:bg-white after:rounded-full checked:after:translate-x-full after:transition-all after:duration-200 after:ease-in disabled:opacity-50 disabled:cursor-not-allowed dark:after:bg-white checked:before:bg-stone-800 checked:after:border-stone-800"
+          checked={isDarkMode}
+          onChange={(e) => setIsDarkMode(e.target.checked) }
         />
-        <p className="text-sm font-medium text-stone-600 dark:text-stone-300">Dark Mode</p>
+        <p className="text-sm font-bold text-stone-600 dark:text-stone-300">
+          {isDarkMode? 'Light Mode': 'Dark Mode'}
+        </p>
       </div>
     </div>
   );
