6684ce10885a7d4cb76c50e96fc6580e35a3e4ea
YuJin020303 <ekfzha8020@gmail.com>
2025-09-27 16:57:53 +0900

    feat: useTask로 상태 관리

--- week02/mission/Chap2_mission_01/src/components/TaskList.tsx
+++ week02/mission/Chap2_mission_01/src/components/TaskList.tsx
@@ -1,14 +1,51 @@
-import type {TTask} from "../types/task";
+import type { TTask } from "../types/task";
 import "../App.css";
+import { useTask } from "../hooks/useTask";
 
 type TaskListProps = {
-  list: TTask[];
   title: string;
   isDone: boolean;
-  createTaskElement: (task: TTask, isDone: boolean) => React.ReactNode;
 };
 
-export const TaskList = ({list, title, isDone, createTaskElement}: TaskListProps) => {
+export const TaskList = ({ title, isDone }: TaskListProps) => {
+  const { todoList, doneList, setTodoList, setDoneList } = useTask();
+  const list = isDone ? doneList : todoList;
+
+  // 할 일 상태 변경 (완료로 이동)
+  const completeTask = (task: TTask): void => {
+    const changedList = todoList.filter((t) => t.id !== task.id);
+    setTodoList(changedList);
+    setDoneList([...doneList, task]);
+  };
+
+  // 완료된 할 일 삭제 함수
+  const deleteTask = (task: TTask): void => {
+    const changedDoneTasks = doneList.filter((t) => t.id !== task.id);
+    setDoneList(changedDoneTasks);
+  };
+
+  // 할 일 아이템 생성 함수
+  const createTaskElement = (task: TTask, isDone: boolean): React.ReactNode => {
+    return (
+      <li key={task.id} className="render-container__item">
+        {task.text}
+        <button
+          className="render-container__item-button"
+          style={{ backgroundColor: isDone ? "#dc3545" : "#28a745" }}
+          onClick={() => {
+            if (isDone) {
+              deleteTask(task);
+            } else {
+              completeTask(task);
+            }
+          }}
+        >
+          {isDone ? "삭제" : "완료"}
+        </button>
+      </li>
+    );
+  };
+
   return (
     <div>
       <div className="render-container__section">
