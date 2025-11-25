b00115590641012d8a3eb6cf50dd12cf121b4b31
YuJin020303 <ekfzha8020@gmail.com>
2025-11-25 23:31:45 +0900

    etc: 9주차 미션3 누락 부분 추가(Provider 삭제)

--- week09/mission/Chap9_mission/src/App.tsx
+++ week09/mission/Chap9_mission/src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css";
-import { Provider } from "react-redux";
 import { CartList } from "./components/CartList";
 import { Navbar } from "./components/Navbar";
-import store from "./store/store";
 
 function App() {
   return (
-    <Provider store={store}>
+    <div>
       <Navbar />
       <CartList />
-    </Provider>
+    </div>
   );
 }
 
